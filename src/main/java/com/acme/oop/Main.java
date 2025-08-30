b6e6cc0ad5faa68f35c567356bedeb23664fbb16
Angel Velasquez <pcisavel@upc.edu.pe>
2025-08-29 22:33:57 -0500

    feat(main): add main behavior.

--- src/main/java/com/acme/oop/Main.java
+++ src/main/java/com/acme/oop/Main.java
@@ -1,9 +1,23 @@
 package com.acme.oop;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.acme.oop.crm.domain.model.aggregates.Customer;
+import com.acme.oop.shared.domain.model.valueobjects.Address;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        // Shared context
+        Address address = new Address("Street", "123", "City", "12345", "USA");
+        System.out.println("First Address: " + address);
+        Address anotherAddress = new Address("Street", "456", "Anytown", "12345", "USA");
+        System.out.println("Second Address: " + anotherAddress);
+        // CRM context
+        System.out.println("Creating a customer...");
+        Customer customer = new Customer("John Doe", "john.doe@gmail.com", address);
+        System.out.println("Customer contact info: " + customer.getContactInfo());
+        System.out.println("Updating customer contact info...");
+        customer.updateContactInfo(customer.getEmail(), anotherAddress);
+        System.out.println("Customer contact info: " + customer.getContactInfo());
+
     }
 }
